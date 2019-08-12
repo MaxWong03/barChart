a5a43d71e876fa63ad35e2e346310fb7fde37d24
MaxWong03 <madmaxmmajournal@gmail.com>
2019-08-12 14:38:07 -0700

    implemented getBarNum addBarTag

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-!(function(){
+const domObject = {
+  chart: '.chart'
+}
+
+
+
+
+
+$(function(){
+  //getBarNum returns the number of bar the chart should render 
+  const getBarNum = (data) => {
+    return data.length;
+  } 
+  
+  //addBarTag add html tag for the bars 
+  const addBarTag = (data) => {
+    const barNum = getBarNum(data);
+    for (let i = 0; i < barNum; i++){
+      $(domObject.chart).append(`<div class=bar${i+1}></div>`);
+    } 
+  }
+  
+  
+  
+  const drawBartChart = (data, option, element) => {
+    addBarTag(data);
+  }
+  
+  drawBartChart([1,2,3,4,5]);
+
 
 });
