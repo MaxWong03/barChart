0a5679cf98e2119eeef30a38bf28a3f89001ea41
MaxWong03 <madmaxmmajournal@gmail.com>
2019-08-12 15:27:57 -0700

    aligned bars to the middle

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const domObject = {
-  chart: '.chart'
+  chart: '.chart',
 }
 
 
@@ -18,19 +18,31 @@ $(function(){
     for (let i = 0; i < barNum; i++){
       $(domObject.chart).append(`<div class=bar${i+1}></div>`);
     } 
-    return barNum+1; //num of column 
+    return barNum+2; //num of column, + 1 for ticks, + 1 for extra white space
   }
   
+  //alignBar puts the bars between ticks and last white space column
+  const alignBar = (charColNum) => {
+    for (let i = 0; i < charColNum - 2 ; i++){
+      let currentBar = `.bar${i+1}`;
+      $(currentBar).css('grid-column', `${i+2}/${i+3}`);
+    } 
+    
+  }
+  //makeChartCol set the number of columns for the chart
   const makeChartCol = (charColNum) => {
     $(domObject.chart).css('grid-template-columns', `repeat(${charColNum}, 1fr)`);
   }
   
+
+
   const drawBartChart = (data, option, element) => {
     const charColNum = addBarTag(data);
     makeChartCol(charColNum);
+    alignBar(charColNum);
   }
   
-  drawBartChart([1,2,3,4,5]);
+  drawBartChart([1,2,3]);
 
 
 });
