2e4412d35fb5df4076861f11ee6ce66a59296765
MaxWong03 <madmaxmmajournal@gmail.com>
2019-08-13 16:12:21 -0700

    bar color now changable

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ $(function () {
     return (Math.max(...data));
   }
 
-  const calPercent = (value,data) => {
-    let percent = (( value/findMax(data) ).toFixed(2))*100;
+  const calPercent = (value, data) => {
+    let percent = ((value / findMax(data)).toFixed(2)) * 100;
     console.log(percent);
     percent === 100 ? percent = 2 : percent = 100 - percent;
     return percent;
@@ -50,7 +50,7 @@ $(function () {
   const addLabel = (data) => {
     const barNum = getBarNum(data);
     for (let i = 0; i < barNum; i++) {
-      $(domObject.chart).append(`<div class=label${i + 1}>Label${i + 1}</div>`);
+      $(domObject.chart).append(`<div class=label${i + 1}>Label-${i + 1}</div>`);
     }
   }
 
@@ -67,38 +67,50 @@ $(function () {
 
   //fillBar fill out the Bars accoording to the input values 
   const fillBar = (data) => {
-    for (let i = 0; i < data.length; i++){
-      let fill = calPercent(data[i],data);
-      let currentBar = `.bar${i+1}`;
-      $(currentBar).css('grid-row', `${fill}/100`); 
+    for (let i = 0; i < data.length; i++) {
+      let fill = calPercent(data[i], data);
+      let currentBar = `.bar${i + 1}`;
+      $(currentBar).css('grid-row', `${fill}/100`);
     }
   }
 
   //displayBarValue display the data value for the corresponding bar
-  const displayBarValue = (data) =>{
-    for (let i = 0; i < data.length; i++){
-      let currentBar = document.querySelector(`.bar${i+1}`);
+  const displayBarValue = (data) => {
+    for (let i = 0; i < data.length; i++) {
+      let currentBar = document.querySelector(`.bar${i + 1}`);
       currentBar.innerHTML = `<p>${data[i]}</p>`;
 
     }
-  } 
+  }
+
+  //barColorOdd changes the color of bars
+  const changeBarColor = (data, color, option) => {
+    let optionValue;
+    option === 'even' ? optionValue = 2 : optionValue = 1;
+    for (let i = optionValue; i < data.length; i += 2) {
+      let currentBar = document.querySelector(`.bar${i}`);
+      $(currentBar).css('background-color', color);
+    }
+
+  }
+
 
   //changeValuePlacement changes the placement of bar's value
   const changeValuePlacement = (placement) => {
-    switch (placement){
+    switch (placement) {
       case 'top':
         $(domObject.allBars).css('align-items', 'flex-start');
         break;
       case 'bottom':
-        $(domObject.allBars).css('align-items', 'flex-end'); 
-        break; 
+        $(domObject.allBars).css('align-items', 'flex-end');
+        break;
       default:
         return 0;  //default is center
     }
-    
-    }
-    
-  
+
+  }
+
+
 
   const drawBarChart = (data, option, element) => {
     const charColNum = addBarTag(data);
@@ -108,9 +120,10 @@ $(function () {
     alignLabel(charColNum);
     fillBar(data);
     displayBarValue(data);
+    changeBarColor(data, 'blue', 'even');
   }
 
-  drawBarChart([300,150,40,10,70,50,200,80,300,20]);
+  drawBarChart([300, 150, 40, 10, 70, 50, 200, 80, 300, 20]);
 
 
-    });
+});
